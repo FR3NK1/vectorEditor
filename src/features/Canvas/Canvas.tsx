a414d2b0532aed8239de4d56f0238f4b3fe43f2a
Dmitriy Mironov <mironov_2001@mail.ru>
2024-02-13 16:40:17 +0300

    Настроил redux slice

--- src/features/Canvas/Canvas.tsx
+++ src/features/Canvas/Canvas.tsx
@@ -1,5 +1,31 @@
+import { useEffect } from 'react'
+import { useSelector } from 'react-redux'
+import { useAppDispatch } from '../../app/hooks/hooks'
+import { IRootState } from '../../app/store/store'
+import { decrement, increment } from './api/CanvasSlice'
+
 const Canvas = () => {
-  return <div style={{ width: '1000px', height: '600px', backgroundColor: 'white' }}>123</div>
+  const count = useSelector((state: IRootState) => state.canvas.value)
+  const dispatch = useAppDispatch()
+
+  useEffect(() => {
+    console.log(count)
+  }, [count])
+  return (
+    <div style={{ width: '1000px', height: '600px', backgroundColor: 'white' }}>
+      <div>
+        <div>
+          <button aria-label='Increment value' onClick={() => dispatch(increment())}>
+            Increment
+          </button>
+          <span>{count}</span>
+          <button aria-label='Decrement value' onClick={() => dispatch(decrement())}>
+            Decrement
+          </button>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Canvas
---
+++ src/features/Canvas/api/CanvasSlice.ts
@@ -0,0 +1,35 @@
+import type { PayloadAction } from '@reduxjs/toolkit'
+import { createSlice } from '@reduxjs/toolkit'
+
+export interface CanvasState {
+  value: number
+}
+
+const initialState: CanvasState = {
+  value: 0,
+}
+
+export const CanvasSlice = createSlice({
+  name: 'Canvas',
+  initialState,
+  reducers: {
+    increment: (state) => {
+      // Redux Toolkit allows us to write "mutating" logic in reducers. It
+      // doesn't actually mutate the state because it uses the Immer library,
+      // which detects changes to a "draft state" and produces a brand new
+      // immutable state based off those changes
+      state.value += 1
+    },
+    decrement: (state) => {
+      state.value -= 1
+    },
+    incrementByAmount: (state, action: PayloadAction<number>) => {
+      state.value += action.payload
+    },
+  },
+})
+
+// Action creators are generated for each case reducer function
+export const { increment, decrement, incrementByAmount } = CanvasSlice.actions
+
+export const canvasReducer = CanvasSlice.reducer
