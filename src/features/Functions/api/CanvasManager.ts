7c175065a067a6f753469f8ebf2340d1ae15d63b
Dmitriy Mironov <mironov_2001@mail.ru>
2024-02-15 15:46:42 +0300

    Добавление фигур

--- src/features/Functions/api/CanvasManager.ts
+++ src/features/Functions/api/CanvasManager.ts
@@ -5,33 +5,42 @@ class CanvasManager {
 
   public setCanvas() {
     this.canvas = new fabric.Canvas('canvas', {
-      height: 1000,
-      width: 1000,
+      height: 500,
+      width: 500,
       backgroundColor: 'white',
       selection: true,
     })
   }
   public addRect() {
     const rect = new fabric.Rect({
-      left: 100,
-      top: 100,
       fill: 'red',
       width: 200,
       height: 200,
     })
     if (this.canvas) {
       this.canvas.add(rect)
+      rect.center()
     }
   }
   public addCircle() {
     const circle = new fabric.Circle({
-      left: 100,
-      top: 100,
       fill: 'green',
       radius: 100,
     })
     if (this.canvas) {
       this.canvas.add(circle)
+      circle.center()
+    }
+  }
+  public addTriangle() {
+    const triangle = new fabric.Triangle({
+      fill: 'blue',
+      width: 200,
+      height: 200,
+    })
+    if (this.canvas) {
+      this.canvas.add(triangle)
+      triangle.center()
     }
   }
   public changeCanvasSize(width: number, height: number) {
--- src/features/Functions/ui/CreateShapeButton.tsx
+++ src/features/Functions/ui/CreateShapeButton.tsx
@@ -1,4 +1,4 @@
-import { ActionIcon, Button, Drawer } from '@mantine/core'
+import { ActionIcon, Drawer, Grid } from '@mantine/core'
 import { useDisclosure } from '@mantine/hooks'
 import { IconShape } from '@tabler/icons-react'
 import { canvasManager } from '../api/CanvasManager'
@@ -9,8 +9,49 @@ const CreateShapeButton = () => {
   return (
     <>
       <Drawer opened={opened} onClose={close} title='Добавить фигуру'>
-        <Button onClick={() => canvasManager.addRect()}>Добавить квадрат</Button>
-        <Button onClick={() => canvasManager.addCircle()}>Добавить круг</Button>
+        <Grid>
+          <Grid.Col span={6}>
+            <ActionIcon
+              variant='transparent'
+              color='orange'
+              style={{
+                width: '100%',
+                height: '100%',
+                aspectRatio: '1 / 1',
+                backgroundColor: 'orange',
+              }}
+              onClick={() => canvasManager.addRect()}
+            />
+          </Grid.Col>
+          <Grid.Col span={6}>
+            <ActionIcon
+              variant='transparent'
+              color='orange'
+              style={{
+                width: '100%',
+                height: '100%',
+                aspectRatio: '1 / 1',
+                backgroundColor: 'orange',
+                clipPath: 'polygon(50% 0%, 0% 100%, 100% 100%)',
+              }}
+              onClick={() => canvasManager.addTriangle()}
+            />
+          </Grid.Col>
+          <Grid.Col span={6}>
+            <ActionIcon
+              variant='transparent'
+              color='orange'
+              style={{
+                width: '100%',
+                height: '100%',
+                aspectRatio: '1 / 1',
+                backgroundColor: 'orange',
+                clipPath: 'circle(50% at 50% 50%)',
+              }}
+              onClick={() => canvasManager.addCircle()}
+            />
+          </Grid.Col>
+        </Grid>
       </Drawer>
       <ActionIcon variant='light' color='orange' aria-label='Shape' size='xl' onClick={open}>
         <IconShape />
