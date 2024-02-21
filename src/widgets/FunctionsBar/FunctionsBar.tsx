a9ed1f9d60f1fc010035c519e838560a0fe3df1c
Dmitriy Mironov <mironov_2001@mail.ru>
2024-02-21 21:26:38 +0300

    Изменил padding

--- src/widgets/FunctionsBar/FunctionsBar.tsx
+++ src/widgets/FunctionsBar/FunctionsBar.tsx
@@ -4,7 +4,7 @@ import CreateShapeButton from '../../features/Functions/ui/CreateShapeButton'
 import classes from './FunctionsBar.module.css'
 const FunctionsBar = () => {
   return (
-    <Stack className={classes.functionsBar} align='center' justify='flex-start'>
+    <Stack className={classes.functionsBar} align='center' justify='flex-start' gap='xs'>
       <ActionIcon variant='light' color='orange' aria-label='Text' size='xl'>
         <IconTextSize />
       </ActionIcon>
